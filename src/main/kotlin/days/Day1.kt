49f69e603d492eb9f6f1ff7583f4dac146d4cb7b
Christopher Luu <nuudles@gmail.com>
2020-12-01 21:32:23 -0500

    Add Day 1 solution

--- src/main/kotlin/days/Day1.kt
+++ src/main/kotlin/days/Day1.kt
@@ -3,15 +3,29 @@ package days
 class Day1 : Day(1) {
 
     override fun partOne(): Any {
-        return inputList.take(2)
-            .map { it.toUpperCase() }
-            .joinToString(" ")
+        val set = HashSet<Int>()
+        return inputList
+            .mapNotNull { it.toIntOrNull() }
+            .forEach { entry ->
+                if (set.contains(2020 - entry)) {
+                    return entry * (2020 - entry)
+                }
+                set.add(entry)
+            }
     }
 
     override fun partTwo(): Any {
-        return inputString.split("\n")
-            .filterNot { it.isEmpty() }
-            .map { it.toUpperCase() }
-            .last()
+        val entries = inputList.mapNotNull { it.toIntOrNull() }
+        entries.forEachIndexed { index, entry ->
+            val set = HashSet<Int>()
+            entries.forEachIndexed forEachIndexed2@ { index2, entry2 ->
+                if (index == index2) return@forEachIndexed2
+                if (set.contains(2020 - entry - entry2)) {
+                    return entry * entry2 * (2020 - entry - entry2)
+                }
+                set.add(entry2)
+            }
+        }
+        return Unit
     }
 }
